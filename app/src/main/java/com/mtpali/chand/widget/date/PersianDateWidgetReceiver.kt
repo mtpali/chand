c4e50b3a2031166832b9d0e4ce0e7558763e2616
MTPALI <alimtp@atomicmail.io>
2026-08-15 11:47:34 +0330

    fix: redraw date widget on launcher resize

--- app/src/main/java/com/mtpali/chand/widget/date/PersianDateWidgetReceiver.kt
+++ app/src/main/java/com/mtpali/chand/widget/date/PersianDateWidgetReceiver.kt
@@ -3,10 +3,21 @@ package com.mtpali.chand.widget.date
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Bundle
 import com.mtpali.chand.widget.WidgetRenderer
 
 class PersianDateWidgetReceiver : AppWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         WidgetRenderer.updateDate(context, appWidgetManager, appWidgetIds)
+    }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        newOptions: Bundle
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+        WidgetRenderer.updateDate(context, appWidgetManager, intArrayOf(appWidgetId))
     }
 }
