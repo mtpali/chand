784bd02cd1be6328b21eb8476bc2a63481ea8b7a
MTPALI <alimtp@atomicmail.io>
2026-08-15 23:07:47 +0330

    chore: bump version 1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         applicationId = "com.mtpali.chand"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.5.3"
+        versionCode = 17
+        versionName = "1.5.4"
 
         buildConfigField("String", "ALANCHAND_TOKEN", "\"$alanChandToken\"")
         buildConfigField("String", "ALANCHAND_API_URL", "\"https://api.alanchand.com\"")
